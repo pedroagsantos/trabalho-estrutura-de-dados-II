f292db20d4a48cce011deb1f434793ed168d3449
pedroagsantos <pedroaguisarsantos1@gmail.com>
2014-01-31 18:59:46 -0200

    Commit inicial com arquivos do professor

---
+++ ED2_Simples/src/Main.java
@@ -0,0 +1,27 @@
+import br.ufrrj.im.cc.ed2.join.base.Tupla;
+import br.ufrrj.im.cc.ed2.juncao.HashJoin;
+
+
+public class Main {
+	
+	
+	public static void testaHashJoin(){
+		long tempo = System.currentTimeMillis();
+		HashJoin relation = new HashJoin("Autor", "id", "Livro", "id_autor");
+		relation.open();
+		Tupla tupla;
+		while ((tupla = (Tupla) relation.next()) != null) {
+//			System.out.println("["+tupla.getValorCampo("nome")+"] : "+tupla.getValorCampo("titulo"));
+		}
+		relation.close();
+		tempo = System.currentTimeMillis() - tempo;
+		System.out.println("Junção com Tabela de Dispersão processada em "+tempo+"ms");
+	}
+	
+	
+	public static void main(String[] args) {
+		testaHashJoin();
+		
+	}
+	
+}
